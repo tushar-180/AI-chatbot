ac8dc263952db44698f622669000827708bfa7be
Tushar Maji <tushar.m@empiricinfotech.com>
2026-05-11 13:57:53 +0530

    fix: update Toaster position to top-right in main entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
     
       <ServerStatusProvider>
         <App />
-      <Toaster position="top-center" richColors />
+      <Toaster position="top-right" richColors />
       </ServerStatusProvider>
     </ClerkProvider>
   
