102b27bba3491057b8624633b4cf9256addecde9
Kashyap <Kashyap.k@empiricinfotech.com>
2026-05-12 17:46:35 +0530

    Remove server/dist from version control

--- server/dist/constants/chat.constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAX_HISTORY_MESSAGES = exports.CHAT_TITLE_MAX_LENGTH = exports.DEFAULT_CHAT_TITLE = exports.DEFAULT_AI_PROVIDER = void 0;
-exports.DEFAULT_AI_PROVIDER = "gemini";
-exports.DEFAULT_CHAT_TITLE = "New Chat";
-exports.CHAT_TITLE_MAX_LENGTH = 30;
-exports.MAX_HISTORY_MESSAGES = 10;
