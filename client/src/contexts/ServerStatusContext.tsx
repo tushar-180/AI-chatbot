c1829df2a8f4ce9667d25d6e9eb9ef18b432cb38
Tushar Maji <tushar.m@empiricinfotech.com>
2026-05-12 18:21:31 +0530

    refactor: remove unused API_BASE_URL import from ServerStatusContext

--- client/src/contexts/ServerStatusContext.tsx
+++ client/src/contexts/ServerStatusContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { API_BASE_URL, API_ORIGIN } from "../lib/api";
+import {API_ORIGIN } from "../lib/api";
 
 interface ServerStatusContextType {
   isDown: boolean;
